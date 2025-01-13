f2ef39fc2e4dde1edb08fe74bf5777acb1fc6ef4
KatrinMul <mulhabibul@yandex.ru>
2025-01-13 23:51:53 +0300

    исправления по замечаниям наставника

--- js/renderModalPhoto.js
+++ js/renderModalPhoto.js
@@ -1,8 +1,18 @@
-import { getRandomInteger, isEscapeKey, isEnterKey } from './utils';
+import { isEscapeKey, isEnterKey } from './utils';
 
+const body = document.querySelector('body');
 const photos = document.querySelector('.pictures');
 const photoModalElement = document.querySelector('.big-picture');
 const photoModalCloseElement = photoModalElement.querySelector('.big-picture__cancel');
+const imageElement = photoModalElement.querySelector('.big-picture__img img');
+const likesElement = photoModalElement.querySelector('.likes-count');
+const descriptionElement = photoModalElement.querySelector('.social__caption');
+const commentsCountElement = photoModalElement.querySelector('.social__comment-count');
+const commentsShownElement = commentsCountElement.querySelector('.social__comment-shown-count');
+const commentsTotalElement = commentsCountElement.querySelector('.social__comment-total-count');
+const commentsList = photoModalElement.querySelector('.social__comments');
+const commentsLoader = photoModalElement.querySelector('.social__comments-loader');
+const commentTemplate = commentsList.querySelector('.social__comment');
 
 const onDocumentKeydown = (evt) => {
   if (isEscapeKey(evt)) {
@@ -11,58 +21,64 @@ const onDocumentKeydown = (evt) => {
   }
 };
 
-function openPhotoModal(evt) {
-  if (evt.target.nodeName === 'IMG' || evt.target.nodeName === 'A') {
-    photoModalElement.classList.remove('hidden');
+const showModal = () => {
+  photoModalElement.classList.remove('hidden');
+  body.classList.add('modal-open');
 
-    document.querySelector('body').classList.add('modal-open');
-    const pictureElement = evt.target.parentElement;
+  document.addEventListener('keydown', onDocumentKeydown);
+};
 
-    photoModalElement.querySelector('.big-picture__img').children[0].src = pictureElement.querySelector('.picture__img').src;
-    photoModalElement.querySelector('.social__caption').textContent = pictureElement.querySelector('.picture__img').alt;
-    photoModalElement.querySelector('.likes-count').textContent = pictureElement.querySelector('.picture__likes').textContent;
+const renderComments = (photo) => {
+  commentsList.innerHTML = '';
+  const commentsListFragment = document.createDocumentFragment();
+  photo.comments.forEach((comment) => {
+    const commentElement = commentTemplate.cloneNode(true);
+    const commentImgElement = commentElement.querySelector('.social__picture');
+    commentImgElement.src = comment.avatar;
+    commentImgElement.alt = comment.name;
+    commentElement.querySelector('.social__text').textContent = comment.message;
+    commentsListFragment.appendChild(commentElement);
+  });
+  commentsList.appendChild(commentsListFragment);
 
-    const pictureCommentsCount = pictureElement.querySelector('.picture__comments').textContent;
-    photoModalElement.querySelector('.social__comment-total-count').textContent = pictureCommentsCount;
-    const pictureCommentsShown = getRandomInteger(1, pictureCommentsCount);
-    photoModalElement.querySelector('.social__comment-shown-count').textContent = pictureCommentsShown;
+  // photoModalElement.querySelector('.social__comment-shown-count').textContent = pictureCommentsShown;
 
-    const commentsList = photoModalElement.querySelector('.social__comments');
-    for (let i = commentsList.children.length - 1; i >= 0; i--) {
-      const child = commentsList.children[i];
-      child.parentElement.removeChild(child);
-    }
+  //   const commentsList = photoModalElement.querySelector('.social__comments');
+  //   for (let i = commentsList.children.length - 1; i >= 0; i--) {
+  //     const child = commentsList.children[i];
+  //     child.parentElement.removeChild(child);
+  //   }
 
-    for (let i = 0; i < pictureCommentsShown; i++) {
-      const commentTemplate = document.createElement('li');
-      commentTemplate.classList.add('social__comment');
-      commentTemplate.innerHTML = '<img class="social__picture" src="{{аватар}}" alt="{{имя комментатора}}" width="35" height="35"><p class="social__text">{{текст комментария}}</p>';
 
-      commentsList.appendChild(commentTemplate);
-    }
 
-    // photoModalElement.querySelector('.social__comment-count').classList.add('hidden');
-    // photoModalElement.querySelector('.social__comments-loader').classList.add('hidden');
+  // photoModalElement.querySelector('.social__comment-count').classList.add('hidden');
+  // photoModalElement.querySelector('.social__comments-loader').classList.add('hidden');
+};
 
-    document.addEventListener('keydown', onDocumentKeydown);
-  }
-}
+const renderModal = (photo) => {
+  imageElement.src = photo.url;
+  imageElement.alt = photo.description;
+  descriptionElement.textContent = photo.description;
+  likesElement.textContent = photo.likes;
+  commentsShownElement.textContent = photo.comments.length;
+  commentsTotalElement.textContent = photo.comments.length;
 
-function closePhotoModal() {
+  renderComments(photo);
+};
+
+
+export const openPhotoModal = (data) => {
+  showModal();
+  renderModal(data);
+};
+
+const closePhotoModal = () => {
   photoModalElement.classList.add('hidden');
 
-  document.querySelector('body').classList.remove('modal-open');
+  body.classList.remove('modal-open');
 
   document.removeEventListener('keydown', onDocumentKeydown);
-}
-
-photos.addEventListener('click', openPhotoModal);
-
-photos.addEventListener('keydown', (evt) => {
-  if (isEnterKey(evt)) {
-    openPhotoModal();
-  }
-});
+};
 
 photoModalCloseElement.addEventListener('click', () => {
   closePhotoModal();
